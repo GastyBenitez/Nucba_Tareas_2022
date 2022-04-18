024ae2cc7b3f8d8f3249b04c65f8e46b199081b4
gaston benitez <gastonbenitez0207@gmail.com>
2022-04-18 18:56:30 -0300

    tarea e1

---
+++ E1 Arrays, Funciones, Bucles  Javascript/app.js
@@ -0,0 +1,24 @@
+/*
+Crear un array con, al menos, 10 ingredientes para hacer pizzas 🍕 (puede ser tanto masa como los ingredientes que van sobre la base)
+Crear una función que acepte ese array como parámetro. 
+
+Dentro de la función, debemos lograr lo siguiente: 
+👉 Iterar ese array para lograr que, los ingredientes que tengan una cantidad par de letras, vayan a un array llamado "pares". 
+👉 Hacer lo mismo con los impares, enviándolos al array "impares". 
+👉 Imprimir en consola:  "Los ingredientes pares son: (ingredientes en array pares). 
+Los ingredientes impares son: (ingredientes en array impares)."
+
+*/
+
+
+
+var ingredientes = ["Harina", "Levadura", "Sal", "Aceite", "Tomate", "Cebolla", "Muzzarella", "Oregano", "Jamon", "agua"]
+// console.log(ingredientes.length);
+
+function resultado (ingredientes){
+    let ingredientesPar = ingredientes.filter (i=> i.length  % 2 ==0);
+    let ingredientesImpar = ingredientes.filter (i=> (!i.length % 2 ==0));
+   return console.log("Los ingredientes pares son " +  ingredientesPar + " "  + "Y los ingredientes impares son  " + ingredientesImpar);
+}
+resultado(ingredientes);
+
---
+++ app.js
@@ -0,0 +1,13 @@
+// alert("hola");
+
+// console.log("Hola mi nombre es gastón y tengo 21 años");
+
+var edad = 12;
+
+if (edad =>  18){
+    console.log("Sos mayor de edad");
+    else(edad < 18){
+        console.log("Sos menor de edad");
+    }
+   
+}
