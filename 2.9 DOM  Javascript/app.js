d4a5741042b94974c0ba4e313b9337fce83c7d35
gaston benitez <gastonbenitez0207@gmail.com>
2022-04-20 21:59:51 -0300

    tarea ultima clase

---
+++ 2.9 DOM  Javascript/app.js
@@ -0,0 +1,26 @@
+/*
+
+*Crear un archivo HTML que contenga: 
+-H1.
+-p.
+-div vacío.
+-inputs de tipo text, password, email, number, checkbox.
+
+*Asignar un id a cada elemento.
+*Capturar cada elemento en una variable.
+*Hacer un console.dir() de cada elemento capturado y revisar en el navegador qué devuelve y que cambia cuando modificamos los elementos (Checkbox tildado o destildado, inputs vacíos o con contenido).
+
+
+*/
+
+const h1Hola = document.querySelector(".h1Hola");
+    console.dir(h1Hola);
+
+const parrafo = document.querySelector(".parrafo");
+    console.dir(parrafo);
+
+const divVacio = document.querySelector(".divVacio");
+    console.dir(divVacio);
+
+const input = document.querySelector(".checkbox");
+    console.dir(input);
